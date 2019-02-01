36f95a662aa08c6be07c29ba3673ea9e0e08efe9
Macos <macos@MacosdeMacBook-Pro.local>
2019-02-02 00:25:43 +0800

    面包屑

--- src/router/index.js
+++ src/router/index.js
@@ -9,16 +9,32 @@ Vue.use(Router)
  */
 import Layout from '../views/layout/Layout'
 
+/**
+* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+*                                if not set alwaysShow, only more than one route under the children
+*                                it will becomes nested mode, otherwise not show the root menu
+* redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
+* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* meta : {
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar
+    breadcrumb: false            if false, the item will hidden in breadcrumb(default is true)
+  }
+**/
+
 export const constantRouterMap = [{
         path: '/login',
         component: () =>
-            import ('@/views/login/index')
+            import ('@/views/login/index'),
+        hidden: true
     },
     {
         path: '/',
         component: Layout,
         redirect: '/dashboard',
         name: 'Dashboard',
+        hidden: true,
         childdren: [{
             path: 'dashboard',
             component: () =>
