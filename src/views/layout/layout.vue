92c8c62e36a77521bae0bd5768370f88ce3993a0
perfectSymphony <2066309334@qq.com>
2019-02-25 18:35:05 +0800

    侧导航模块实现

--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,18 +1,20 @@
 
 <template>
 <div class="app-wrapper">
+    <sidebar />
     <div class="main-container">
         <navbar/>
     </div>
 </div>
 </template>
 <script>
-import { Navbar } from './components'
+import { Navbar, Sidebar } from './components'
 
 export default {
     name: 'Layout',
     components: {
-        Navbar
+        Navbar,
+        Sidebar
     },
     computed: {
         
