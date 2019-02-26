8d65af36cb3cca70f0350a918442eb0ca798c4af
perfectSymphony <2066309334@qq.com>
2019-02-27 00:10:05 +0800

    登陆之后的页面布局完成

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -10,7 +10,7 @@
       <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span>导航一</span>
+          <span>Example</span>
         </template>
         <el-menu-item-group>
           <template slot="title">分组一</template>
@@ -27,15 +27,11 @@
       </el-submenu>
       <el-menu-item index="2">
         <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+        <span slot="title">From</span>
       </el-menu-item>
-      <el-menu-item index="3" disabled>
+      <el-menu-item index="3">
         <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
+        <span slot="title">Nested</span>
       </el-menu-item>
     </el-menu>    
 </template>
--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -22,13 +22,13 @@ export default {
 }
 </script>
 <style rel="stylesheet/scss" lang="scss" scoped>
-// @import "src/styles/mixin.scss";
-// .app-wrapper {
-//     @include clearfix;
-//     position: relative;
-//     height: 100%;
-//     width: 100%;
-// }
+@import "src/styles/mixin.scss";
+.app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+}
 
 // .drawer-bg {
 //     background: #000;
