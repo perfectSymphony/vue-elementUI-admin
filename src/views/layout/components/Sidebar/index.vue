52db09ecd99a88ed7252d1c1c709223972eac62f
perfectSymphony <2066309334@qq.com>
2019-03-01 18:43:25 +0800

    单页面开发

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -7,44 +7,20 @@
       :active-text-color="variables.menuActiveText"
      mode="vertical"
      >
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
-        </template>
-        <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
+      <sidebar-item />
     </el-menu> 
   </el-scrollbar>   
 </template>
 <script>
 import { mapGetters } from 'vuex'
 import variables from '@/styles/variables.scss'
+import sidebarItem from './SidebarItem'
 
   export default {
+    name: 'Sidebar',
+    components: {
+      sidebarItem
+    },
     computed: {
       ...mapGetters([
         'sidebar'
