961b6a019074ee4a9002313a42a20174a35f777c
perfectSymphony <2066309334@qq.com>
2019-03-04 18:21:11 +0800

    侧导航菜单开发中...

---
+++ src/views/layout/components/Sidebar/Link.vue
@@ -0,0 +1,41 @@
+<template>
+    <!-- 使用component标签，使用linkProps(to)绑定数据(参数 ‘to’ 中所包含的数据)
+    component标签切换组件
+     -->
+    <component v-bind="linkProps(to)">
+        <!-- <slot/> -->
+    </component>
+</template>
+<script>
+import { isExternal } from '@/utils/validate'
+
+export default {
+    props: {
+        to: {
+            type: String,
+            required: true
+        }
+    },
+
+    methods: {
+        linkProps(url){
+            if(isExternal(url)){
+                return {
+                    is: 'a',
+                    href: url,
+                    target: '_blank',
+                    rel: 'noopener'
+                }
+            }
+            return {
+                is: 'router-link',
+                to: url
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
+
--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,4 +1,13 @@
 <template>
+  <div class="menu-wrapper">
+      <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
+        <app-link :to = "resolvePath(onlyOneChild.path)">
+          <el-menu-item :index="resolvePath(onlyOneChild.path)">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>          
+        </app-link>
+      </template>
       <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-location"></i>
@@ -17,16 +26,58 @@
           <el-menu-item index="1-4-1">选项1</el-menu-item>
         </el-submenu>
       </el-submenu>
-   
+  </div>
 </template>
 <script>
 
 import path from 'path'
+import { isExternal } from '@/utils/validate'
+import AppLink from './Link'
 
 export default {
     name: 'SidebarItem',
     components: {
-
+      AppLink
+    },
+    props: {
+      item: {
+        type: Object,
+        required: true
+      }
+    },
+    data(){
+      this.onlyOneChild = null
+      return {}
+    },
+    methods: {
+      hasOneShowingChild(children = [], parent){
+        const showingChildren = children.filter(item => {
+          console.log(item)
+          if(item.hidden){
+            return false
+          }else{
+            //临时存储（只有一个子节点时）
+            this.onlyOneChild = item
+            return true
+          }
+        })
+        //当只有一个子路由时，则该子路由为默认路由
+        if(showingChildren.length == 1){
+          return true
+        }
+          //没有子路由时，只展示父级路由
+        if(showingChildren.length === 0){
+          this.onlyOneChild = { ... parent, path: '', noShowingChildren: true }
+          return true
+        }
+        return false
+      },
+      resolvePath(routePath){
+        if(isExternal(routePath)){
+          return routePath
+        }
+        return path.resolve(this.basePath, routePath)
+      }
     }
     
 }
