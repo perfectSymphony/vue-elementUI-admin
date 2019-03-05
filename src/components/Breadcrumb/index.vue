c52c58de9f3003ceddae7858f59df4435ffbc437
perfectSymphony <2066309334@qq.com>
2019-03-06 00:29:53 +0800

    ...

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,16 +1,32 @@
 <template>
 <el-breadcrumb class="app-breadcrumb" separator="/">
-  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-  <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-  <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-  <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+  <transition-group name="breadcrumb">
+    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+  </transition-group>
 </el-breadcrumb>   
 </template>
 <script>
 import pathToRegexp from 'path-to-regexp'
 
 export default {
-    
+    data(){
+      return {
+        levelList: null
+      }
+    },
+    watch: {
+      $route(){
+        this.getBreadcrumb()
+      }
+    },
+    created(){
+      this.getBreadcrumb()
+    },
+    methods: {
+      getBreadcrumb(){
+        
+      }
+    }
 }
 </script>
 <style rel="stylesheet/scss" scoped>
--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="menu-wrapper">
+  <div v-if="!item.hidden" class="menu-wrapper">
       <!-- <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
         <app-link :to = "resolvePath(onlyOneChild.path)">
           <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
