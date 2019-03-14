312800cc3c81aa8307e06806abf787ee42dc7d4d
perfectSymphony <2066309334@qq.com>
2019-03-14 16:12:54 +0800

    注释Mock.XHR.prototype.proxy_send函数

--- mock/index.js
+++ mock/index.js
@@ -4,13 +4,14 @@ import tableAPI from './table'
 
 // Fix an issue with setting withCredentials = true, cross-domain request lost cookies  
 // https://github.com/nuysoft/Mock/issues/300
-Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
-Mock.XHR.prototype.send = function() {
-    if (this.custom.xhr) {
-        this.custom.xhr.withCredentials = this.withCredentials || false
-    }
-    this.proxy_send(...arguments)
-}
+// Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
+// Mock.XHR.prototype.send = function() {
+//     if (this.custom.xhr) {
+//         this.custom.xhr.withCredentials = this.withCredentials || false
+//     }
+//     this.proxy_send(...arguments)
+
+// }
 
 //user
 Mock.mock(/\/user\/login/, 'post', userAPI.login)
